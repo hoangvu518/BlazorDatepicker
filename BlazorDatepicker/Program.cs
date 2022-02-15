10678edd21e7355b48ac2b1087023fcefd121195
Hoangvu518 <Hoangvu518@gmail.com>
2022-02-14 23:02:23 -0800

    adding redux dev tools

--- BlazorDatepicker/Program.cs
+++ BlazorDatepicker/Program.cs
@@ -9,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly));
+builder.Services.AddFluxor(options => {
+    options.ScanAssemblies(typeof(Program).Assembly);
+    options.UseReduxDevTools();
+}) ;
 
 var app = builder.Build();
 
